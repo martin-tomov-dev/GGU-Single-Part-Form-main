7de2a2eaf8b0b3bc82495a9f04a1d3b64d524f87
martin-tomov-dev <devsonspree@gmail.com>
2022-05-16 04:40:18 +0100

    no message

--- src/App.vue
+++ src/App.vue
@@ -6,16 +6,16 @@
 </template>
 
 <script>
-    // import SimpleFormPage from './components/simple/SimpleFormPage.vue'
-    // // import DialogMain from './components/dialog/DialogMain.vue'
+    import SimpleFormPage from './components/simple/SimpleFormPage.vue'
+    // import DialogMain from './components/dialog/DialogMain.vue'
 
-    // export default {
-    //     name: 'App',
-    //     components: {
-    //         SimpleFormPage,
-    //         // DialogMain,
-    //     }
-    // }
+    export default {
+        name: 'App',
+        components: {
+            SimpleFormPage,
+            // DialogMain,
+        }
+    }
 </script>
 
 <style src="@/assets/styles/app.css"></style>
